8daa5a2de37e435155750bbc8f2a02501f2ee034
Mikael Siidorow <mikael@siidorow.com>
2026-08-11 17:21:40 +0300

    feat(barcode): validate supported product codes

---
+++ VegaTests/ProductBarcodeTests.swift
@@ -0,0 +1,42 @@
+import Testing
+
+@testable import Vega
+
+struct ProductBarcodeTests {
+    @Test(arguments: [
+        ("96385074", ProductBarcode.Format.ean8),
+        ("012345678905", ProductBarcode.Format.upcA),
+        ("5901234123457", ProductBarcode.Format.ean13),
+        ("10012345000017", ProductBarcode.Format.gtin14),
+    ])
+    func acceptsServerSupportedBarcodeLengths(
+        _ candidate: String,
+        _ expectedFormat: ProductBarcode.Format
+    ) throws {
+        let barcode = try #require(ProductBarcode("  \(candidate)\n"))
+
+        #expect(barcode.value == candidate)
+        #expect(barcode.format == expectedFormat)
+    }
+
+    @Test(arguments: [
+        "1234567",
+        "123456789",
+        "12345678901",
+        "123456789012345",
+        "590123412345x",
+        "",
+    ])
+    func rejectsValuesTheServerDoesNotTreatAsBarcodes(_ candidate: String) {
+        #expect(ProductBarcode(candidate) == nil)
+    }
+
+    @Test
+    func scanGateAcceptsOnlyTheFirstValidRecognition() throws {
+        var gate = BarcodeScanGate()
+
+        #expect(try #require(gate.accept("5901234123457")).value == "5901234123457")
+        #expect(gate.accept("10012345000017") == nil)
+        #expect(gate.acceptedBarcode?.value == "5901234123457")
+    }
+}
